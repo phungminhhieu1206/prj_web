a2c84c1e31fe3f554da11320c983a1281b5e7c6f
Phung Minh Hieu <phungminhhieu1206@gmail.com>
2021-05-26 21:12:54 +0700

    update

--- ismart.com/modules/cart/views/showView.php
+++ ismart.com/modules/cart/views/showView.php
@@ -106,7 +106,6 @@ $list_info = get_info_cart();
         </div>
         <div class="section" id="action-cart-wp">
             <div class="section-detail">
-                <p class="title">Click vào <span>“Cập nhật giỏ hàng”</span> để cập nhật số lượng. Nhập vào số lượng <span>0</span> để xóa sản phẩm khỏi giỏ hàng. Nhấn vào thanh toán để hoàn tất mua hàng.</p>
                 <a href="san-pham.html" title="" id="buy-more">Mua tiếp</a><br/>
                 <a href="xoa-0-d.html" title="" id="delete-cart">Xóa giỏ hàng</a>
             </div>
